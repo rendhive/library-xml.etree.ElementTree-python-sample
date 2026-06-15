41ab50ecc6dc78c75b7291e73ce5f7324e97726b
rendhive <rendhivecode@gmail.com>
2026-06-15 07:39:22 +0700

    [DOCT] Sample Library Python: 1. Mengimpor Modul xml.etree.ElementTree.py

---
+++ 1. Mengimpor Modul xml.etree.ElementTree.py
@@ -0,0 +1,5 @@
+import xml.etree.ElementTree as ET
+
+print("Modul xml.etree.ElementTree berhasil diimpor.")
+# Fungsi: Mengimpor modul untuk menggunakan API Tree untuk memanipulasi XML.
+# Kondisi: Digunakan sebelum memanipulasi atau mem-parsing dokumen XML.
